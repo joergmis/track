197df30f87a5fceb32f1e05e1857046349a168d4
Mischa Jörg <mischa@ninetyfour.ch>
2024-03-30 12:25:51 +0100

    cmd: track: add command to list build version
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

---
+++ cmd/track/cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/joergmis/track"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the build version of the app",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(track.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
