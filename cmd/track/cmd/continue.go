3d7a034d9e3d9ac9284d890c5182c773d5efb447
Mischa Jörg <mischa@ninetyfour.ch>
2024-05-02 21:53:06 +0200

    cmd: continue: continue activities by id
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- cmd/track/cmd/continue.go
+++ cmd/track/cmd/continue.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/joergmis/track"
 	"github.com/spf13/cobra"
@@ -9,15 +10,54 @@ import (
 
 var continueCmd = &cobra.Command{
 	Use:   "continue",
-	Short: "Continue the last activity",
+	Short: "Continue an activity",
+	Long: `By default, the last activity is used as a template for the new 
+activity. Append the id to the command to continue a specific activity. 
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		previousActivity, err := storage.GetLastActivity()
 		if err != nil {
 			log.Fatalf("get last activity: %v", err)
 		}
 
-		// clone the activity since we want an additional entry in the storage
-		newActivity := track.NewActivity(previousActivity.Customer, previousActivity.Project, previousActivity.Service, previousActivity.Description)
+		if previousActivity.InProgress() {
+			previousActivity.Stop()
+			if err := storage.UpdateActivity(previousActivity); err != nil {
+				log.Fatalf("stop last activity: %v", err)
+			}
+		}
+
+		var newActivity track.Activity
+
+		if len(args) == 0 {
+			previousActivity, err := storage.GetLastActivity()
+			if err != nil {
+				log.Fatalf("get last activity: %v", err)
+			}
+
+			newActivity = track.NewActivity(previousActivity.Customer, previousActivity.Project, previousActivity.Service, previousActivity.Description)
+		} else if len(args) == 1 {
+			index, err := strconv.Atoi(args[0])
+			if err != nil {
+				log.Fatalf("parse index of activity to parse: %v", err)
+			}
+
+			activities, err := storage.GetAllActivities()
+			if err != nil {
+				log.Fatalf("get all activities: %v", err)
+			}
+
+			if index > len(activities)+1 {
+				log.Fatalf("index is greater than the number of activities: %d > %d", index, len(activities))
+			}
+
+			previousActivity := activities[index]
+
+			newActivity = track.NewActivity(previousActivity.Customer, previousActivity.Project, previousActivity.Service, previousActivity.Description)
+		} else {
+			log.Fatalf("unsupported number of arguments: %d", len(args))
+		}
+
 		newActivity.Start()
 
 		if err := storage.AddActivity(newActivity); err != nil {
--- cmd/track/cmd/status.go
+++ cmd/track/cmd/status.go
@@ -6,85 +6,100 @@ import (
 	"time"
 
 	"github.com/cheynewallace/tabby"
-	"github.com/joergmis/track"
 	"github.com/spf13/cobra"
 )
 
-var statusCmd = &cobra.Command{
-	Use:   "status",
-	Short: "Get an overview over the timetracking",
-	Run: func(cmd *cobra.Command, args []string) {
-		activities, err := storage.GetAllActivities()
-		if err != nil {
-			log.Fatalf("get all activities from storage: %v", err)
-		}
+var (
+	statusCmdWeekly  bool
+	statusCmdMonthly bool
 
-		inRange := []track.Activity{}
-
-		// only show the current day
-		start := time.Now().Add(-1 * time.Duration(time.Now().Hour()) * time.Hour)
-		end := time.Now().Add(1 * time.Hour)
-
-		for _, entry := range activities {
-			if entry.StartTime.Before(start) || entry.StartTime.After(end) || entry.EndTime.After(end) {
-				continue
+	statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "Get an overview over the timetracking",
+		Long: `By default, it shows the daily overview. Check out '--weekly' or
+'--monthly' if you need to check out previous activities.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			activities, err := storage.GetAllActivities()
+			if err != nil {
+				log.Fatalf("get all activities from storage: %v", err)
 			}
 
-			inRange = append(inRange, entry)
-		}
+			if len(activities) == 0 {
+				fmt.Println("-- no activities in range --")
+				return
+			}
 
-		if len(inRange) == 0 {
-			fmt.Println("-- no activities in range --")
-			return
-		}
+			var start, end time.Time
 
-		t := tabby.New()
-		t.AddHeader("time", "duration", "customer", "project", "description")
+			if statusCmdWeekly {
+				start = time.Now().Add(
+					-time.Duration(time.Now().Hour()) -
+						time.Duration(time.Now().Weekday()*24)*time.Hour)
+			} else if statusCmdMonthly {
+				start = time.Now().Add(
+					-time.Duration(time.Now().Hour()) -
+						time.Duration(time.Now().Day()*24)*time.Hour)
+			} else {
+				start = time.Now().Add(-1 * time.Duration(time.Now().Hour()) * time.Hour)
+			}
 
-		total := time.Duration(0)
+			end = time.Now().Add(1 * time.Hour)
 
-		for i, entry := range inRange {
-			if i > 0 {
-				previous := inRange[i-1]
+			t := tabby.New()
+			t.AddHeader("time", "duration", "id", "customer", "project", "description")
 
-				if entry.StartTime.Sub(previous.EndTime).Minutes() > 5 {
-					pause := entry.EndTime.Sub(previous.StartTime)
-					t.AddLine(
-						fmt.Sprintf("%s - %s", previous.EndTime.Add(1*time.Second).Format(time.TimeOnly), entry.StartTime.Add(-1*time.Second).Format(time.TimeOnly)),
-						fmt.Sprintf("%02d:%02d h", int(pause.Hours()), int(pause.Minutes())%60),
-						"-- pause --",
-						"--",
-						"--",
-					)
+			total := time.Duration(0)
+
+			for i, entry := range activities {
+				if entry.StartTime.Before(start) || entry.StartTime.After(end) || entry.EndTime.After(end) {
+					continue
 				}
+
+				if i > 0 {
+					previous := activities[i-1]
+
+					if entry.StartTime.Sub(previous.EndTime).Minutes() > 5 {
+						pause := entry.EndTime.Sub(previous.StartTime)
+						t.AddLine(
+							fmt.Sprintf("%s - %s", previous.EndTime.Add(1*time.Second).Format(time.TimeOnly), entry.StartTime.Add(-1*time.Second).Format(time.TimeOnly)),
+							fmt.Sprintf("%02d:%02d h", int(pause.Hours()), int(pause.Minutes())%60),
+							"-- pause --",
+							"--",
+							"--",
+						)
+					}
+				}
+
+				total += entry.Duration()
+
+				t.AddLine(
+					fmt.Sprintf(
+						"%s - %s",
+						entry.StartTime.Format(time.TimeOnly),
+						entry.EndTime.Format(time.TimeOnly),
+					),
+					fmt.Sprintf(
+						"%02d:%02d h",
+						int(entry.Duration().Hours()),
+						int(entry.Duration().Minutes())%60,
+					),
+					fmt.Sprintf("%d", i),
+					entry.Customer,
+					entry.Project,
+					entry.Description,
+				)
 			}
 
-			total += entry.Duration()
+			t.AddLine("---", "---")
+			t.AddLine("total", fmt.Sprintf("%02d:%02d h", int(total.Hours()), int(total.Minutes())%60))
 
-			t.AddLine(
-				fmt.Sprintf(
-					"%s - %s",
-					entry.StartTime.Format(time.TimeOnly),
-					entry.EndTime.Format(time.TimeOnly),
-				),
-				fmt.Sprintf(
-					"%02d:%02d h",
-					int(entry.Duration().Hours()),
-					int(entry.Duration().Minutes())%60,
-				),
-				entry.Customer,
-				entry.Project,
-				entry.Description,
-			)
-		}
-
-		t.AddLine("---", "---")
-		t.AddLine("total", fmt.Sprintf("%02d:%02d h", int(total.Hours()), int(total.Minutes())%60))
-
-		t.Print()
-	},
-}
+			t.Print()
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(statusCmd)
+	statusCmd.PersistentFlags().BoolVar(&statusCmdWeekly, "weekly", false, "Show the weekly stats")
+	statusCmd.PersistentFlags().BoolVar(&statusCmdMonthly, "monthly", false, "Show the monthly stats")
 }
